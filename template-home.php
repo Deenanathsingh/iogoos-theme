7a026bbebe7a38dce96bac865f19f3f23294fce2
DEENA NATH SINGH <deenanath.singh.549@gmail.com>
2022-05-20 19:06:36 +0530

    1

--- template-home.php
+++ template-home.php
@@ -2,10 +2,9 @@
 <html>
 
 <head>
-    <title>
-        watch
-    </title>
+    <title> watch </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/image/icon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
