82e2f32dc4bcd30e3253e3ff25d60bda37f106c3
DEENA NATH SINGH <deenanath.singh.549@gmail.com>
2022-05-24 17:57:27 +0530

    f

--- footer.php
+++ footer.php
@@ -195,7 +195,7 @@
     </div>
 </footer>
 <!-- footer -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 <?php wp_footer(); ?>
